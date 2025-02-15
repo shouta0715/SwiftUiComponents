35675e9962c5a909094332beebf1c34ebbbd0171
shouta0715 <kshouta0715@gmail.com>
2025-02-15 20:45:30 +0900

    ForEach View

---
+++ SwiftUiComponents/Lists/ForEachView.swift
@@ -0,0 +1,49 @@
+//
+//  ForEachView.swift
+//  SwiftUiComponents
+//
+//  Created by k21047kk on 2025/02/15.
+//
+
+import SwiftUI
+
+struct ForEachView: View {
+  @Environment(ApplicationData.self) private var app
+  
+    var body: some View {
+      VStack {
+        ForEach(app.userData) { book in
+          VStack {
+            HStack(alignment: .top) {
+              Image(book.cover)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 100)
+              
+              VStack(alignment: .leading) {
+                Text(book.title)
+                  .bold()
+                
+                Text(book.author)
+                  
+                Text(book.displayYear)
+                  .font(.caption)
+              }
+              .padding(.top, 5)
+              
+              Spacer()
+            }
+            
+            Divider()
+          }
+        }
+      }
+      .padding(.horizontal, 10)
+      Spacer()
+    }
+}
+
+#Preview {
+    ForEachView()
+    .environment(ApplicationData.shared)
+}
--- SwiftUiComponents/SwiftUiComponentsApp.swift
+++ SwiftUiComponents/SwiftUiComponentsApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct SwiftUiComponentsApp: App {
+  @State private var app = ApplicationData.shared
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+            .environment(app)
         }
     }
 }
